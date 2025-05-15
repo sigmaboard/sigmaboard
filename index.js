9b090612efaee3ba03abcd0d2aae41110ce64b59
BigThiccy2 <safalingdown@gmail.com>
2025-05-15 14:56:16 -0500

    pp

---
+++ index.js
@@ -0,0 +1,11 @@
+ <script>
+    function scrollDown() {
+      window.scrollBy({
+        top: 500, // The number of pixels to scroll down (adjust as needed)
+        left: 0,
+        behavior: 'smooth' // Smooth scrolling effect
+      });
+    }
+  </script>
+</body>
+</html>
